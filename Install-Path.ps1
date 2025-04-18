80d13c37f8fa23346b3997162dbe68fd398a6ef0
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-04-18 12:25:15 +0700

    Update Install-Path.ps1

--- Install-Path.ps1
+++ Install-Path.ps1
@@ -1,9 +1,7 @@
 $myPaths = "$HOME\.cargo\bin;" +
         "C:\tools\msys64\usr\bin;" +
         "C:\tools\neovim\nvim-win64\bin;" +
-        "$HOME\AppData\Local\JetBrains\Toolbox\scripts;" +
-        "C:\ProgramData\chocolatey\lib\Elixir\tools\bin;"
-
+        "$HOME\AppData\Local\JetBrains\Toolbox\scripts;" 
 
 $paths = $myPaths + $env:Path -split ";"
 $pathSet = New-Object 'System.Collections.Generic.HashSet[string]'
